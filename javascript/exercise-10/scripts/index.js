bb390e4ed2c6d677a5766189f5e657f5a8a86058
Khoi Le <minarek@outlook.com>
2018-02-05 12:44:33 +0100

    Fix bugs to exercises 10

--- javascript/exercise-10/scripts/index.js
+++ javascript/exercise-10/scripts/index.js
@@ -1,6 +1,6 @@
-var minID = 1;
-var maxID = 3;
-var randomNumber = getRandomNumber(minID,maxID);
+var minNumber = 1;
+var maxNumber = 3;
+var randomNumber = getRandomNumber(minNumber,maxNumber);
 
 /*document.getElementById("displayRandomNumber").innerHTML = randomNumber;*/
 
@@ -9,11 +9,15 @@ btnCheckNumber.addEventListener("click", function () {
     let txtNumberInput = convertStringToNumber(document.getElementById("txtNumberInput").value);
     if (txtNumberInput === randomNumber){
         alert("Your answer is right!");
-    }else if (txtNumberInput < minID){
+    }else if (txtNumberInput < minNumber){
         alert("Your answer is false! IT'S TOO LOW!");
-    }else if (txtNumberInput > maxID){
+    }else if (txtNumberInput > maxNumber){
         alert("Your answer is false! IT'S TOO HIGH!");
     }else{
         alert("Your answer is false!");
     }
+});
+
+document.getElementById("btnPlayAgain").addEventListener("click", function () {
+   location.reload();
 });
